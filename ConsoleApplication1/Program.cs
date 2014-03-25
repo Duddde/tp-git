c9878ca02acb2a61c33e616a5f44c25bb23d986e
sytha <sytha38@gmail.com>
2014-03-25 17:19:26 +0100

    Commit de la version fonctionnelle

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,13 +13,23 @@ namespace LOTO
             Console.WriteLine("----------*    ****       *     **** ***** ****----------");
             Console.WriteLine("----------*    ***        *     *  *   *   *  *----------");
             Console.WriteLine("----------**** ****       ****  ****   *   ****----------\n");
+            Console.WriteLine("---------------------------------------------------------\n");
             DateTime date = DateTime.Now;
-            Console.WriteLine("----------Tirage du "+date.Day+"----------\n");
+            Console.WriteLine("-------------------Tirage du " + date.Day + "/" + date.Month + "/" + date.Year + "-------------------\n\n");
             int[] tabloto = new int[6];
+            Random rand = new Random();
             for (int i = 0; i < 6; i++)
             {
-
+                int number=rand.Next(1, 49);
+                if (tabloto.Contains(number))
+                {
+                    i--;
+                    continue;
+                }
+                tabloto[i] = number;
+                Console.Write(number+"\t");
             }
+            Console.ReadKey();
         }
     }
 }
